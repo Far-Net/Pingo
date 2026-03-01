1c5156d24b0861bd09d14c87320aa2d7629f48c9
farnet <farnet001@gmail.com>
2026-03-01 16:05:19 -0300

    Update Pingo.py

--- Pingo.py
+++ Pingo.py
@@ -4,23 +4,30 @@ from sys import stdout
 from time import sleep
 from pyfiglet import figlet_format
 
+print('I N I T I N G  P I N G O...')
+sleep(3)
+system('clear')
+
+print(Style.BRIGHT + Fore.CYAN + 'A long time ago in galaxy far, \nfar away....')
+sleep(4)
+system('clear')
+
+init(autoreset = True)
 
 def carregando():
     for i in range(3):
         for chars in ['*', '&', '#', '+']:
-            stdout.write(Style.BRIGHT + Fore.WHITE + '\rCarregando ' + chars.ljust(4))
+            stdout.write(Style.BRIGHT + Fore.WHITE + '\rCARREGANDO ' + chars.ljust(4))
             stdout.flush()
             sleep(0.3)
 
-init(autoreset = True)
 
 def pingo():
 
         #INICIANDO
         system('clear')
         banner = figlet_format('PINGO!', font = 'lean')
-        print(Style.BRIGHT + Fore.GREEN + banner)
-        print('')
+        print(Style.RESET_ALL + banner, '\n')
 
         continuar = input('| Deseja continuar? (s/n): ').strip()
         while continuar == '':
@@ -29,26 +36,23 @@ def pingo():
 
         if continuar == 'sim' or continuar == 'ss' or continuar == 's':
             print('')
-            print(Style.BRIGHT + Fore.CYAN + '[ INICIANDO ]')
+            print(Style.BRIGHT + Fore.CYAN + '[ INICIANDO ] \n')
             carregando()
             system('clear')
 
-            print(Style.RESET_ALL + '-'*70)
-            print('')
+            print(Style.RESET_ALL + '-'*70, '\n')
 
             #ENDEREÇO WEB
             web = input('| Indique um endereço web -> ').strip()
 
             while web == web.upper() or web == '':
                 print('')
-                print(Style.BRIGHT + Fore.RED + '[ ERRO FATAL (endereço não especificado...) ]')
-                print('')
+                print(Style.BRIGHT + Fore.RED + '[ ERRO FATAL (endereço não especificado...) ] \n')
                 web = input('| Indique um endereço web -> ').strip()
 
             while not web.endswith('.com'):
                 print('')
-                print(Style.BRIGHT + Fore.RED + '[ ERRO FATAL (o endereço web não possui .com) ]')
-                print('')
+                print(Style.BRIGHT + Fore.RED + '[ ERRO FATAL (o endereço web não possui .com) ] \n')
                 web = input('| Indique um endereço web -> ').strip()
 
 
@@ -60,15 +64,13 @@ def pingo():
 
                     while contagem == '':
                         print('')
-                        print(Style.BRIGHT + Fore.RED + '[ ERRO FATAL (número de pacotes não especificado...) ]')
-                        print('')
+                        print(Style.BRIGHT + Fore.RED + '[ ERRO FATAL (número de pacotes não especificado...) ] \n')
                         contagem = int(input('| Quantos pacotes deseja mandar para {}? '.format(Style.BRIGHT + Fore.BLUE + web)))
                         break
 
                 except ValueError:
                     print('')
-                    print(Style.BRIGHT + Fore.RED + '[ ERRO (a contagem de pacotes não suporta letras ou pacotes vazios...) ]')
-                    print('')
+                    print(Style.BRIGHT + Fore.RED + '[ ERRO (a contagem de pacotes não suporta letras ou pacotes vazios...) ] \n')
 
             if contagem == 66:
                 print('')
@@ -81,26 +83,21 @@ def pingo():
                 sleep(5)
 
             system('clear')
-            print(Style.RESET_ALL + '-'*70) #DEIXANDO AS LINHAS BRANCAS
-            print('')
+            print(Style.RESET_ALL + '-'*70, '\n') #DEIXANDO AS LINHAS BRANCAS
 
             carregando()
 
             system('clear')
-            print(Style.RESET_ALL + ' CHECANDO '.center(100, '-'))
-            print('')
+            print(Style.RESET_ALL + ' CHECANDO '.center(100, '-'), '\n')
 
             #SAÍDA DE DADOS ATRIBUÍDOS ACIMA
             print('| Endereço web -> {}'.format(Style.BRIGHT + Fore.GREEN + web))
             if contagem == 66:
-                print('| Quantidade de pacotes -> {}'.format(Style.BRIGHT + Fore.MAGENTA + str(contagem)))
+                print('| Quantidade de pacotes -> {}'.format(Style.BRIGHT + Fore.MAGENTA + str(contagem)), '\n')
             else:
-                print('| Quantidade de pacotes -> {}'.format(Style.BRIGHT + Fore.GREEN + str(contagem)))
+                print('| Quantidade de pacotes -> {}'.format(Style.BRIGHT + Fore.GREEN + str(contagem)),'\n')
 
-
-            print('')
-            print('-'*100)
-            print('')
+            print('-'*100, '\n')
 
             #PING
             if contagem > 20:
@@ -109,105 +106,95 @@ def pingo():
                 system('ping -c {} {}'.format(contagem, web))
 
             print('')
-            print('-'*100)
-            print('')
+            print('-'*100, '\n')
 
             #TEMPO DE ENVIO
             while True:
                 tempo = str(input('| Qunto foi o tempo de envio de pacotes? (time): ')).strip()
                 if tempo == '':
-                    print(Style.BRIGHT + Fore.YELLOW + '> TEMPO INDEFINIDO')
+                    print(Style.BRIGHT + Fore.YELLOW + '> TEMPO INDEFINIDO \n')
                     break
                 try:
                     tempo_float = float(tempo)
                     if float(tempo_float) < 20:
-                        print(Style.BRIGHT + Fore.CYAN + '> Conexão ótima')
+                        print(Style.BRIGHT + Fore.CYAN + '> Conexão ótima \n')
                         break
                     elif float(tempo_float) >= 20 and tempo_float <= 50:
-                        print(Style.BRIGHT + Fore.GREEN + '> Conexão boa')
+                        print(Style.BRIGHT + Fore.GREEN + '> Conexão boa \n')
                         break
                     elif float(tempo_float) >= 50 and tempo_float <= 100:
-                        print(Style.BRIGHT + Fore.YELLOW + '> Conexão aceitável')
+                        print(Style.BRIGHT + Fore.YELLOW + '> Conexão aceitável \n')
                         break
                     elif float(tempo_float) >= 150:
-                        print(Style.BRIGHT + Fore.RED + '> Conexão ruim')
+                        print(Style.BRIGHT + Fore.RED + '> Conexão ruim \n')
                         break
                 except ValueError:
-                        print('')
-                        print(Style.BRIGHT + Fore.RED + '< VALOR INVÁLIDO >')
-                        print('')
+                        print(Style.BRIGHT + Fore.RED + '< VALOR INVÁLIDO > \n')
 
-            print('')
 
             #PACOTES PERDIDOS
             while True:
                 pacote_los = str(input('| Quantos pacotes foram perdidos? (packet loss): ')).strip()
 
                 if pacote_los == '':
-                    print(Style.BRIGHT + Fore.YELLOW + '> PACOTES INDEFINIDOS')
+                    print(Style.BRIGHT + Fore.YELLOW + '> PACOTES INDEFINIDOS \n')
                     break
                 try:
                     if int(pacote_los) > 0:
-                        print(Style.BRIGHT + Fore.RED + '> Instabilidade na rede')
+                        print(Style.BRIGHT + Fore.RED + '> Instabilidade na rede \n')
                         break
                     else:
-                        print(Style.BRIGHT + Fore.GREEN + '> A rede não apresenta instabilidades')
+                        print(Style.BRIGHT + Fore.GREEN + '> A rede não apresenta instabilidades \n')
                         break
                 except ValueError:
-                    print('')
-                    print(Style.BRIGHT + Fore.RED + '< VALOR INVÁLIDO >')
-                    print('')
+                    print(Style.BRIGHT + Fore.RED + '< VALOR INVÁLIDO > \n')
 
-            print('')
-            print('-'*100)
-            print('')
+            print('-'*100, '\n')
 
             #RELATORIO
             relatiorio = input('| Gostaria de ver o relatório? (s/n) ').strip()
             if relatiorio == 's' or relatiorio == 'ss' or relatiorio == 'sim':
                 print('')
-                print(Style.BRIGHT + Fore.MAGENTA + '[ INICIANDO RELATÓRIO ]')
+                print(Style.BRIGHT + Fore.YELLOW + '[ INICIANDO RELATÓRIO ] \n')
                 carregando()
                 system('clear')
 
-                print(Style.RESET_ALL + ' RELATÓRIO '.center(40, '-'))
-                print('')
+                print(Style.RESET_ALL + ' RELATÓRIO '.center(40, '-'), '\n')
 
-                print('| Endereço web alvo -> {}'.format(Style.BRIGHT + Fore.GREEN + web))
+                print('| Endereço web alvo -> {}'.format(Style.BRIGHT + Fore.GREEN + web), '\n')
                 if contagem == 66:
-                    print('| Pacotes enviados -> {}'.format(Style.BRIGHT + Fore.MAGENTA + str(contagem)))
+                    print('| Pacotes enviados -> {}'.format(Style.BRIGHT + Fore.MAGENTA + str(contagem)), '\n')
                 else:
-                    print('| Pacotes enviados -> {}'.format(Style.BRIGHT + Fore.GREEN + str(contagem)))
+                    print('| Pacotes enviados -> {}'.format(Style.BRIGHT + Fore.GREEN + str(contagem)), '\n')
 
                 tempo_relatorio = str(tempo)
 
                 if tempo_relatorio == '':
-                    print(Style.BRIGHT + Fore.YELLOW + '> TEMPO INDEFINIDO')
+                    print(Style.BRIGHT + Fore.YELLOW + '> TEMPO INDEFINIDO \n')
                 else:
                     tempo_relatorio_float = float(tempo_relatorio)
 
                     if float(tempo_relatorio_float < 20):
-                        print('| Tempo -> {}ms, Conexão ótima'.format(Style.BRIGHT + Fore.CYAN + str(tempo_relatorio_float)))
+                        print('| Tempo -> {}ms, Conexão ótima'.format(Style.BRIGHT + Fore.CYAN + str(tempo_relatorio_float)), '\n')
                     elif float(tempo_relatorio_float >= 20 and tempo_relatorio_float <= 50):
-                        print('| Tempo -> {}ms, Conexão boa'.format(Style.BRIGHT + Fore.GREEN + str(tempo_relatorio_float)))
+                        print('| Tempo -> {}ms, Conexão boa'.format(Style.BRIGHT + Fore.GREEN + str(tempo_relatorio_float)), '\n')
                     elif float(tempo_relatorio_float >= 50 and tempo_relatorio_float <= 100):
-                        print('| Tempo -> {}ms, Conexão aceitável'.format(Style.BRIGHT + Fore.YELLOW + str(tempo_relatorio_float)))
+                        print('| Tempo -> {}ms, Conexão aceitável'.format(Style.BRIGHT + Fore.YELLOW + str(tempo_relatorio_float)), '\n')
                     elif float(tempo_relatorio_float > 150):
-                        print('| Tempo -> {}ms, Conexão ruim'.format(Style.BRIGHT + Fore.RED + str(tempo_relatorio_float)))
+                        print('| Tempo -> {}ms, Conexão ruim'.format(Style.BRIGHT + Fore.RED + str(tempo_relatorio_float)), '\n')
 
                 pacote_relatorio = str(pacote_los)
 
                 if pacote_relatorio == '':
-                    print(Style.BRIGHT + Fore.YELLOW + '> PACOTES INDEFINIDOS')
+                    print(Style.BRIGHT + Fore.YELLOW + '> PACOTES INDEFINIDOS \n')
                 else:
                     pacote_relatorio_int = int(pacote_relatorio)
                     if pacote_relatorio_int > 0:
-                        print('| Pacotes perdids -> {}, Rede instável'.format(Style.BRIGHT + Fore.RED + str(pacote_relatorio_int)))
+                        print('| Pacotes perdids -> {}%, Rede instável'.format(Style.BRIGHT + Fore.RED + str(pacote_relatorio_int)), '\n')
                     else:
-                        print('| Pacotes perdidos -> {}, Rede estável'.format(Style.BRIGHT + Fore.GREEN + str(pacote_los)))
+                        print('| Pacotes perdidos -> {}%, Rede estável'.format(Style.BRIGHT + Fore.GREEN + str(pacote_los)), '\n')
 
-                print('')
-                print('-'*40)
+                print('-'*40, '\n')
 
             #LOOP
             reset = input('| Quer recomeçar? (s/n): ').strip()
@@ -218,52 +205,14 @@ def pingo():
                 pingo()
             else:
                 print('')
-                print(Style.BRIGHT + Fore.RED + '[ SAINDO ]')
+                print(Style.BRIGHT + Fore.RED + '[ SAINDO ] \n')
                 carregando()
                 system('clear')
         else:
             print('')
-            print(Style.BRIGHT + Fore.RED + '[ FECHANDO ]')
+            print(Style.BRIGHT + Fore.RED + '[ FECHANDO ] \n')
             carregando()
             system('clear')
 pingo()
 
-print(Style.BRIGHT + Fore.WHITE + '''
-
-                              ++##@@@@@@@@@@@@@@##--
-                          @@@@@@@@@@@@::@@::@@@@@@@@@@##
-                      @@@@@@##      MM@@@@::        ##@@@@mm
-                    @@@@@@        @@@@@@@@@@@@@@        ##@@##
-                  @@@@            ##@@@@@@@@@@##          --@@@@
-                @@@@            ::@@@@@@@@@@@@MM              @@@@
-              @@@@  ::      ::@@@@@@@@@@@@@@@@@@@@@@          ..@@##
-            @@@@  mm@@MM  @@@@@@@@@@--      --@@@@@@@@@@  MM@@  @@@@--
-            @@@@  @@@@@@@@@@@@@@@@@@        ++@@@@@@@@@@##@@@@@@++@@@@
-          ##@@@@@@@@@@@@@@@@@@@@@@@@MM      ##@@@@@@@@@@@@@@@@@@@@##@@mm
-          @@##  @@@@@@@@@@@@MM@@@@@@@@      @@@@@@@@  MM@@@@@@@@@@  @@@@
-        ++@@  ##@@@@@@@@##    MM@@@@@@      @@@@@@      ##@@@@@@@@##..@@
-        ##@@  @@@@@@@@@@        @@..                      @@@@@@@@@@  @@mm
-        @@@@      @@@@@@                                  @@@@MM      @@##
-        @@mm      @@@@@@@@                            ##@@@@@@##      @@@@
-        @@        @@@@@@@@@@@@MM                  ##@@@@@@@@@@@@      @@@@
-        @@        @@@@@@@@@@@@mm                  @@@@@@@@@@@@@@      @@@@
-        @@        @@@@@@@@@@@@MM                  ##@@@@@@@@@@@@      @@@@
-        @@@@      ##@@@@MM                              ::@@@@##      @@@@
-        @@@@      ##@@@@                                  @@@@MM      @@##
-        ##@@  @@@@@@@@@@        @@--          @@@@      ..@@@@@@@@@@  @@++
-        --@@  MM@@@@@@@@##    mm@@@@@@      @@@@@@mm    @@@@@@@@@@::--@@
-          @@##  @@@@@@@@@@@@MM@@@@@@##      @@@@@@@@MM@@@@@@@@@@@@  @@@@
-          ##@@@@@@@@@@@@@@@@@@@@@@@@MM      MM@@@@@@@@@@@@@@@@@@@@@@@@..
-            @@@@  @@@@@@@@@@@@@@@@@@          @@@@@@@@@@##@@@@@@..@@@@
-            mm@@mm  @@    MM@@@@@@@@##......##@@@@@@@@mm  MM@@  ##@@
-              ##@@            ##@@@@@@@@@@@@@@@@@@@@          ::@@@@
-                @@@@..            ##@@@@@@@@@@MM            ::@@##
-                  @@@@mm          @@@@@@@@@@@@MM          ##@@##
-                    ##@@##        @@@@@@@@@@@@@@        @@@@@@
-                      ::@@@@@@..        @@..      --@@@@@@
-                          mm@@@@@@@@@@##@@@@@@@@@@@@@@..
-                                mm##@@@@@@@@@@##++
-''')
-print(Style.BRIGHT + Fore.RED + 'ALL FOR THE EMPIRE')
-sleep(2)
-system('clear')
+print(Style.BRIGHT + Fore.RED + 'ALL FOR THE EMPIRE!')
